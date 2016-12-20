1096332ec3533ef9212df406b57dc46151db735e
marko <markotaht@gmail.com>
2016-12-20 19:21:06 +0200

    Better octree..

--- OctTree.js
+++ OctTree.js
@@ -1,12 +1,10 @@
 
 
-var OctTree = function(x,y,z,width, height, depth, objects){
+var OctTree = function(x,y,z,width, objects){
 	this.x = x;
 	this.y = y;
 	this.z = z;
 	this.width = width;
-	this.height = height;
-	this.depth = depth;
 	this.children = [];
 	this.objects = objects;
 	this.MAX_OBJECTS = 5;
@@ -14,9 +12,9 @@ var OctTree = function(x,y,z,width, height, depth, objects){
 }
 
 OctTree.prototype.contains = function(object){
-	if(obj[0] > this.x && obj[0] < this.x + this.width){
-		if(obj[1] > this.y && obj[1] < this.y + this.height){
-			if(obj[2] > this.z && obj[2] < this.z + this.depth){
+	if(obj[0] > this.x && obj[0] <= this.x + this.width){
+		if(obj[1] > this.y && obj[1] <= this.y + this.width){
+			if(obj[2] > this.z && obj[2] <= this.z + this.width){
 				return true;
 			}		
 			return false;
@@ -26,17 +24,26 @@ OctTree.prototype.contains = function(object){
 	return false;
 }
 
+OctTree.prototype.addObject(object){
+	for(var i = 0; i < 8; i++){
+		if(this.children[i].contains(object)){
+			this.children.objects.push(object);
+			break;
+		}
+	}
+}
+
 
 OctTree.prototype.doStep = function(){
 	if(this.objects.length > this.MAX_OBJECTS){
-		this.children[0] = new OctTree(x,y,z,width/2,height/2,depth/2);
-		this.children[1] = new OctTree(x+width/2,y,z,width/2,height/2,depth/2);
-		this.children[2] = new OctTree(x,y,z+depth/2,width/2,height/2,depth/2);
-		this.children[3] = new OctTree(x+width/2,y,z+depth/2,width/2,height/2,depth/2);
-		this.children[4] = new OctTree(x,y+height/2,z,width/2,height/2,depth/2);
-		this.children[5] = new OctTree(x+width/2,y+height/2,z,width/2,height/2,depth/2);
-		this.children[6] = new OctTree(x,y+height/2,z+depth/2,width/2,height/2,depth/2);
-		this.children[7] = new OctTree(x+width/2,y+height/2,z+depth/2,width/2,height/2,depth/2);
+		this.children[0] = new OctTree(x,y,z,width/2);
+		this.children[1] = new OctTree(x+width/2,y,z,width/2);
+		this.children[2] = new OctTree(x,y,z+depth/2,width/2);
+		this.children[3] = new OctTree(x+width/2,y,z+depth/2,width/2);
+		this.children[4] = new OctTree(x,y+height/2,z,width/2);
+		this.children[5] = new OctTree(x+width/2,y+height/2,z,width/2);
+		this.children[6] = new OctTree(x,y+height/2,z+depth/2,width/2);
+		this.children[7] = new OctTree(x+width/2,y+height/2,z+depth/2,width/2);
 
 		
 		var temp = JSON.parse(JSON.stringify(this.objects));
@@ -56,7 +63,12 @@ OctTree.prototype.doStep = function(){
 	//Done, no more partitioning needed
 	return false;
 }
+//FUncion to put this object to right subcube.
+OctTree.prototype.NearestNeighbor = function(object){
+	this.NNpoint = object;
+}
 
-OctTree.prototype.NearestNeighbor = function(){
+//Chec subcubes get current closest
+OctTree.prototype.doNNStep() = function(){
 	
 }
