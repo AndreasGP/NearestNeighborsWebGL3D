81770d4693b70e953061134db086cd3c88b117cc
marko <markotaht@gmail.com>
2016-12-20 17:26:02 +0200

    Generates octree stepby step. Needs full initial set of points.

--- OctTree.js
+++ OctTree.js
@@ -1,6 +1,6 @@
 
 
-var OctTree = function(x,y,z,width, height, depth){
+var OctTree = function(x,y,z,width, height, depth, objects){
 	this.x = x;
 	this.y = y;
 	this.z = z;
@@ -8,17 +8,15 @@ var OctTree = function(x,y,z,width, height, depth){
 	this.height = height;
 	this.depth = depth;
 	this.children = [];
-	this.objects = [];
-	this.recursive_depth = 0;
-	this.MIN_OBJECTS = 2;
+	this.objects = objects;
 	this.MAX_OBJECTS = 5;
-	this.MAX_DEPTH = 8;
+	this.NNpoint = [];
 }
 
 OctTree.prototype.contains = function(object){
-	if(obj.x > this.x && obj.x < this.x + this.width){
-		if(obj.y > this.y && obj.y < this.y + this.height){
-			if(obj.z > this.z && obj.z < this.z + this.depth){
+	if(obj[0] > this.x && obj[0] < this.x + this.width){
+		if(obj[1] > this.y && obj[1] < this.y + this.height){
+			if(obj[2] > this.z && obj[2] < this.z + this.depth){
 				return true;
 			}		
 			return false;
@@ -28,27 +26,9 @@ OctTree.prototype.contains = function(object){
 	return false;
 }
 
-OctTree.prototype.addObject = function(object){
-	//Vaja parandada veel
-	if(this.contains(object){
-		if(this.children.length != 0){
-			for(int i = 0; i < 8; i++){
-				if(this.children[i].contains(object)){
-					this.children[i].addObject(object);
-					return true;
-				}
-			}
-		}else{
-			this.objects.push(object)
-			this.checkFullness();
-			return true;
-		}
-	}
-	return false;
-}
 
-OctTree.prototype.checkFullness(){
-	if(this.objects.length > this.MAX_OBJECTS && recursive_depth < MAX_DEPTH){
+OctTree.prototype.doStep = function(){
+	if(this.objects.length > this.MAX_OBJECTS){
 		this.children[0] = new OctTree(x,y,z,width/2,height/2,depth/2);
 		this.children[1] = new OctTree(x+width/2,y,z,width/2,height/2,depth/2);
 		this.children[2] = new OctTree(x,y,z+depth/2,width/2,height/2,depth/2);
@@ -57,10 +37,7 @@ OctTree.prototype.checkFullness(){
 		this.children[5] = new OctTree(x+width/2,y+height/2,z,width/2,height/2,depth/2);
 		this.children[6] = new OctTree(x,y+height/2,z+depth/2,width/2,height/2,depth/2);
 		this.children[7] = new OctTree(x+width/2,y+height/2,z+depth/2,width/2,height/2,depth/2);
-		
-		for(int i = 0; i < 8; i++){
-			this.children[i].recursive_depth += 1;
-		}
+
 		
 		var temp = JSON.parse(JSON.stringify(this.objects));
 		
@@ -68,6 +45,18 @@ OctTree.prototype.checkFullness(){
 		
 		for(var i = 0; i < temp.length; i++){
 			this.addObject(temp[i]);
+		return true;
+	}else{
+		for(var i = 0; i < 8; i++){
+			if(this.children[i].doStep()){
+				return true;
+			}
 		}
 	}
+	//Done, no more partitioning needed
+	return false;
+}
+
+OctTree.prototype.NearestNeighbor = function(){
+	
 }
