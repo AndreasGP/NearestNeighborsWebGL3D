7147e375dbd5e75d764aafb4f70db32cd609bd6f
AndreasGP <aaatttsss@hotmail.com>
2017-01-09 20:35:46 +0200

    Added polygon drawing

--- visualizer.js
+++ visualizer.js
@@ -182,10 +182,6 @@ function drawCube(pos, size, color) {
     return wireframe;
 }
 
-function removeFromScene(object) {
-    scene.remove(object);
-}
-
 function drawSphere(pos, radius, color, transparent, opacity) {
     var geometry = new THREE.SphereGeometry(radius, 20, 8);
     var material = new THREE.MeshBasicMaterial({
@@ -201,6 +197,47 @@ function drawSphere(pos, radius, color, transparent, opacity) {
     return sphere;
 }
 
+//Only supports convex shapes, 3 vertices minimum. Vertices have to be ordered properly.
+//Example:
+//drawPolygon([[2, 0, 0], [2, 5, 0], [2, 5, 8], [2, 3, 9], [2, 0, 8]], 0xff0000, 0xaa0000, true, 0.2);
+function drawPolygon(vertices, fillColor, edgeColor, transparent, opacity) {
+    var material = new THREE.MeshBasicMaterial({
+        color: fillColor,
+        side: THREE.DoubleSide,
+        transparent: transparent,
+        opacity: opacity
+    });
+    var geometry = new THREE.Geometry();
+
+    for(var i = 0; i < vertices.length; i++) {
+        var renderP = pointSpaceTo3DRenderSpace(vertices[i]);
+        var vec = new THREE.Vector3(renderP[0], renderP[1], renderP[2]);
+        console.log(vec);
+        geometry.vertices.push(vec);
+    }
+    
+    for (var i = 1; i < vertices.length - 1; i++) {        
+        var face = new THREE.Face3(0, i, i + 1);
+        geometry.faces.push(face);
+    }
+
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+    
+    //Reusing the same variables
+    var material = new THREE.LineBasicMaterial({
+        color: edgeColor
+    });
+    var geometry = new THREE.EdgesGeometry(geometry);
+    var outline = new THREE.LineSegments(geometry, material);
+    scene.add(outline);
+    
+    return [mesh, outline]
+}
+
+function removeFromScene(object) {
+    scene.remove(object);
+}
 /* END FUNCTIONS FOR RENDERING */
 
 /* BEGIN RENDERING CALLS AND MAIN LOGIC */
